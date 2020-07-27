cc36760f45a1cf64e0bbb38e49bd3d76df32e026
Rostislav086 <Rostislav.086@mail.ru>
2020-07-27 21:40:36 +0500

    First commit on the branch

---
+++ script/main.js
@@ -0,0 +1,26 @@
+"use strict";
+
+import { radioPlayerInit } from "./radioPlayer.js";
+
+// получаем элементы со страницы ==============================================
+const playerBtn = document.querySelectorAll(".player-btn"),
+  playerBlock = document.querySelectorAll(".player-block"),
+  temp = document.querySelector(".temp");
+
+// Определяем функции =========================================================
+const deactivationPlayer = () => {
+  temp.style.display = "none";
+  playerBtn.forEach((item) => item.classList.remove("active"));
+  playerBlock.forEach((item) => item.classList.remove("active"));
+};
+// Обработчики событий ========================================================
+
+// Вызов функций ==============================================================
+
+playerBtn.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    deactivationPlayer();
+    btn.classList.add("active");
+    playerBlock[index].classList.add("active");
+  });
+});
